b94a72752a94b97e8038c45d9e0a360311f21017
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-06-03 23:17:08 +0300

    add support %p

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,13 @@
 /*   By: mtriston <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/31 20:58:58 by mtriston          #+#    #+#             */
-/*   Updated: 2020/06/03 00:20:50 by mtriston         ###   ########.fr       */
+/*   Updated: 2020/06/03 23:15:41 by mtriston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include "libft/libft.h"
 #include <stdio.h>
-
-/* int	num_of_arguments(const char *str)
-{
-	int		num_of_arguments;
-	char	*s;
-
-	num_of_arguments = 0;
-	s = (char *)str;
-	while (s != NULL)
-		if (s = ft_strchr(s, '%'))
-		{
-			num_of_arguments++;
-			s++;
-		}
-	return (num_of_arguments);
-}
-*/
 
 static char	*print_argument(char *str, va_list ap)
 {
@@ -42,9 +25,14 @@ static char	*print_argument(char *str, va_list ap)
 	else if (*str == '%')
 		ft_putchar_fd('%', 1);
 	else if (*str == 'x')
-		ft_putnbr_base(va_arg(ap, size_t), "0123456789abcdef");
+		ft_putunbr_base(va_arg(ap, size_t), "0123456789abcdef");
 	else if (*str == 'X')
-		ft_putnbr_base(va_arg(ap, size_t), "0123456789ABCDEF");
+		ft_putunbr_base(va_arg(ap, size_t), "0123456789ABCDEF");
+	else if (*str == 'p')
+	{
+		ft_putstr_fd("0x", 1);
+		ft_putunbr_base(va_arg(ap, intptr_t), "0123456789abcdef");
+	}
 	return (str + 1);
 }
 
@@ -58,9 +46,7 @@ int			ft_printf(const char *format, ...)
 	while (*str != '\0')
 	{
 		if (*str == '%')
-		{
 			str = print_argument(++str, ap);
-		}
 		else
 			ft_putchar_fd(*str++, 1);
 		
@@ -70,13 +56,15 @@ int			ft_printf(const char *format, ...)
 
 int		main()
 {
+	int a;
 	//char *name = "Alex";
 	//char *name2 = "Anna";
 //printf("%p\n", &name);
 	//ft_printf("Hello, %s and %s. You are %d great persons%c100%%\n", name, name2, x, '!');
 	//ft_putnbr_fd(-2147483648, 1);
 	//ft_putchar_fd('\n', 1);
-	ft_printf("%x\n", 15);
-	printf("%x", 15);
+//	ft_printf("%x\n", 15);
+	printf("%p\n", &a);
+	ft_printf("%p", &a);
 	return 0;
 }
